dc963b664dc17d9fe66fb8246b1e24419f7bd968
Bole05 <bole.wu@estudiantat.upc.edu>
2025-03-31 11:16:41 +0200

    c

--- src/RenderComponent.cpp
+++
@@ -1,6 +0,0 @@
-#include "RenderComponent.h"
-#include "raylib.h"
-
-void RenderComponent::DrawRectangleEntity(Rectangle rect, Color color) {
-    DrawRectangleRec(rect, color);
-}
--- src/Sprite.cpp
+++
@@ -1,9 +0,0 @@
-#include "Sprite.h"
-
-Sprite::Sprite(Texture2D texture) {
-    this->texture = texture;
-}
-
-void Sprite::Draw(float x, float y) {
-    DrawTexture(texture, x, y, WHITE);
-}
